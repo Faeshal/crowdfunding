2929c3be36266f7e9861a721c7a6fe2e1354a2e6
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2022-06-04 15:24:19 +0700

    add campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,32 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(userID int) ([]Campaign, error)
+}
+
+// * butuh dependecy ke repository
+type service struct {
+	repository Repository
+}
+
+// * supaya bisa di call dari luar package
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) FindCampaigns(userID int) ([]Campaign, error) {
+
+	if userID != 0 {
+		campaigns, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+		return campaigns, nil
+	}
+
+	campaigns, err := s.repository.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
