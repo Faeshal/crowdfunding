4aaacd2db15a83ae93300b22ff1b003c5c368c74
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2022-06-05 13:21:53 +0700

    prepare transaction domain

---
+++ transaction/entity.go
@@ -0,0 +1,21 @@
+package transaction
+
+import (
+	"crowdfunding/campaign"
+	"crowdfunding/user"
+	"time"
+)
+
+type Transaction struct {
+	ID         int
+	CampaignID int
+	UserID     int
+	Amount     int
+	Status     string
+	Code       string
+	PaymentURL string
+	User       user.User
+	Campaign   campaign.Campaign
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
